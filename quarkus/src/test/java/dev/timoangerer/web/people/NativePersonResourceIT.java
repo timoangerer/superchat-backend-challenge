b2e6ef0394144401228d1ff3728ba992c57eab80
timoangerer <timo.angerer@code.berlin>
2021-03-26 09:26:49 +0100

    Setup basic project structure

---
+++ quarkus/src/test/java/dev/timoangerer/web/people/NativePersonResourceIT.java
@@ -0,0 +1,9 @@
+package dev.timoangerer.web.people;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePersonResourceIT extends PersonResourceTest {
+
+    // Execute the same tests but in native mode.
+}
