d5b38ed142336456075594464bbdbc90b5d6131d
timoangerer <timo.angerer@code.berlin>
2021-03-27 21:10:13 +0100

    Create create new contact recieved by WA webhook

--- quarkus/src/main/java/dev/timoangerer/channel/whatsapp/db/WhatsappChannelRepository.java
+++ quarkus/src/main/java/dev/timoangerer/channel/whatsapp/db/WhatsappChannelRepository.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 
 import dev.timoangerer.contact.model.Contact;
 import dev.timoangerer.core.db.PersistenceException;
+import dev.timoangerer.webhooks.whatsapp.model.WhatsappMessage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,8 +20,9 @@ public class WhatsappChannelRepository {
 
     private static final String FIND_CONTACT_BY_ID = "select contacts.* from whatsapp_channels inner join channels on channels.id = whatsapp_channels.channel_id inner join contacts on contacts.id = channels.contact_id where wa_id = ?";
 
-    private final DataSource dataSource;
+    private static final String INSERT = "with first_insert as ( insert into channels (contact_id) values (?) returning id as channel_id ) insert into whatsapp_channels (channel_id, wa_id) values ( (select channel_id from first_insert), ?)";
 
+    private final DataSource dataSource;
     public WhatsappChannelRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
@@ -40,5 +42,18 @@ public class WhatsappChannelRepository {
         }
         return null;
 
+    }
+
+    //TODO Using waId instead of an object. But WhatsappMessage object would be missleading. Better create a dedicated class for wa channel
+    public void insert(Contact contact, String waId) {
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(INSERT)) {
+            statement.setObject(1, contact.getId());
+            statement.setString(2, waId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.print(e);
+            throw new PersistenceException(e);
+        }
     }
 }
--- quarkus/src/main/java/dev/timoangerer/webhooks/whatsapp/web/WhatsappMessageResource.java
+++ quarkus/src/main/java/dev/timoangerer/webhooks/whatsapp/web/WhatsappMessageResource.java
@@ -1,9 +1,12 @@
 package dev.timoangerer.webhooks.whatsapp.web;
 
+import java.util.UUID;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import dev.timoangerer.channel.whatsapp.db.WhatsappChannelRepository;
+import dev.timoangerer.contact.db.ContactRepository;
 import dev.timoangerer.contact.model.Contact;
 import dev.timoangerer.webhooks.whatsapp.model.WhatsappMessage;
 
@@ -12,27 +15,27 @@ public class WhatsappMessageResource {
 
     private final WhatsappChannelRepository whatsappChannelRepository;
 
-    public WhatsappMessageResource(WhatsappChannelRepository whatsappChannelRepository) {
+    private final ContactRepository contactRepository;
+
+    public WhatsappMessageResource(WhatsappChannelRepository whatsappChannelRepository, ContactRepository contactRepository) {
       this.whatsappChannelRepository = whatsappChannelRepository;
+      this.contactRepository = contactRepository;
     }
 
     @POST
     public void receiveMessage(WhatsappMessage whatsappMessage) {
-        System.out.println("Received wa message:");
-        System.out.println(whatsappMessage.getText());
 
-        // Find or create customer based on recieved message
         Contact contact = whatsappChannelRepository.findContactByWaId(whatsappMessage.getWaId());
+        
         if (contact == null) {
-            System.out.println("no contact associated with this wa_id");
-            
-        } else {
-            System.out.println("Found a contact!");
-            System.out.println(contact.getName());
+            Contact newContact = new Contact(null, whatsappMessage.getProfileName(), null);
+            contact = contactRepository.insert(newContact);
+
+            whatsappChannelRepository.insert(contact, whatsappMessage.getWaId());
+            System.out.println("Created new contact from new WA channel");
         }
 
-        // Query the webhook specifc table (WA in this case) for the identifier (waId)
-        // If there is a waId, fetch the customer associated with it.
-        // Else, create a new customer with the infos of the channel.
+        System.out.println("Contact from WA endpoint:");
+        System.out.println(contact.getName());
     }
 }
